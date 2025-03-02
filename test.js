6b4beb5c04fe1fdfb435a847becb77ff32560a45
Evian-Chen <paramecium128@gmail.com>
2025-03-03 01:42:13 +0800

    feat: test google map api successufully

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { connectDB } from "./lib/mongodb.js";
 import mongoose from "mongoose";
 import axios from "axios";
 
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const GOOGLE_API_KEY = process.env.GOOGLE_MAP_API_KEY;
 const LOCATION = "25.0330,121.5654";
 const RADIUS = 5000;
 
@@ -22,14 +22,19 @@ async function testDB() {
 }
 
 async function testGoogleAPI() {
-    const url =`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LOCATION}&radius=${RADIUS}&type=cafe&key=${GOOGLE_API_KEY}`;
-    try {
-        const response = await axios.get(url);
-        console.log("google api return: ", response.data);
-    } catch (err) {
-        console.log("error: ", err);
-    }
+  console.log("Google API Key:", GOOGLE_API_KEY);
+
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LOCATION}&radius=${RADIUS}&type=cafe&key=${GOOGLE_API_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    console.log("google request: ", response.data.results[0]);
+  } catch (error) {
+    console.error("failed connection: ", error.message);
+  }
 }
+
+
 
 // testDB();
 testGoogleAPI();
