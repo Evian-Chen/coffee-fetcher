57d106cb5f7f1b81c141a02ebcda6437215ad0a2
EvianChen <paramecium128@gmail.com>
2025-03-14 17:46:53 +0800

    feat: copy all data from mongoDB to firebase. add init_firebase.js and firebase.js

---
+++ firebase.js
@@ -0,0 +1,62 @@
+import { getDiscrits } from "./getDiscrits.js";
+import { connectCityDB } from "./lib/mongodb.js";
+import mongoose from "mongoose";
+import CoffeeShopSchema from "./models/CoffeeShop.js";
+import { db } from "./init_firebase.js";
+import { ref, update } from "firebase/database";
+
+async function getShopData() {
+    // 取得縣市與行政區，結束後關閉連線
+    const regions = await getDiscrits();
+    await mongoose.connection.close();
+
+    for (const [city, districts] of Object.entries(regions)) {
+        // 取得目前縣市的連線
+        const cityConn = await connectCityDB(city);
+
+        for (const district of districts) {
+            // 把縣市底下所有行政區的資料都撈出來
+            let cityModel = cityConn.model(district, CoffeeShopSchema);
+            const shops = await cityModel.find();
+
+            const updates = {};
+            for (const shop of shops) {
+                // 使用 Google Places API 的 place_id 作為 key
+                const shopName = shop.name.replace(/[\s.#$/\[\]]+/g, "");
+                const shopId = `${shopName}_${shop.place_id}`; 
+            
+                // 把資料放在路徑底下
+                updates[`/coffee_shops/${city}/${district}/${shopId}`] = {
+                    district: shop.district,
+                    city: shop.city,
+                    name: shop.name,
+                    vicinity: shop.vicinity,
+                    rating: shop.rating,
+                    price_level: shop.price_level || null,
+                    weekday_text: shop.weekday_text,
+                    formatted_address: shop.formatted_address,
+                    formatted_phone_number: shop.formatted_phone_number || null,
+                    services: shop.services,
+                    types: shop.types || [],
+                    user_rating_total: shop.user_rating_total || 0,
+                    reviews: shop.reviews ? JSON.parse(JSON.stringify(shop.reviews)) : [],
+                    createdAt: shop.createdAt.toISOString(),
+                    updatedAt: shop.updatedAt.toISOString(),
+                };
+            
+                console.log(`${city} -> ${district}: ${shop.name}`);
+            
+                // 不覆蓋資料
+                await update(ref(db), updates); 
+                
+            }
+        }
+        
+    }
+
+    // 關閉所有連線
+    await mongoose.connection.close();
+}
+
+var data = await getShopData();
+console.log(data);
--- init_firebase.js
+++ init_firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +15,7 @@ const firebaseConfig = {
   authDomain: "coffee-fetcher-e3938.firebaseapp.com",
   projectId: "coffee-fetcher-e3938",
   storageBucket: "coffee-fetcher-e3938.firebasestorage.app",
+  databaseURL: "https://coffee-fetcher-e3938-default-rtdb.firebaseio.com/",
   messagingSenderId: FIREBASE_MESSAGE_SENDER_ID,
   appId: FIREBASE_APP_ID,
   measurementId: "G-Y133TH7Y8Y"
@@ -22,3 +23,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
