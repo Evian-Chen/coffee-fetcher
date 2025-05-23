370ad060ed7a6e001025029dd49bbc87bc550b22
EvianChen <paramecium128@gmail.com>
2025-05-23 16:02:54 +0800

    feat: add expandDataset

--- FireBase/expandDataset.js
+++ FireBase/expandDataset.js
@@ -1,441 +1,151 @@
-import { collection, addDoc } from "firebase/firestore"; 
 import { db } from "./init_firestore.js"
-
-const CITIES = [
-    "臺北市",
-      "新北市",
-      "基隆市",
-      "桃園市",
-      "新竹縣",
-      "新竹市",
-      "苗栗縣",
-      "臺中市",
-      "南投縣",
-      "彰化縣",
-      "雲林縣",
-      "嘉義縣",
-      "嘉義市",
-      "臺南市",
-      "高雄市",
-      "屏東縣",
-      "宜蘭縣",
-      "花蓮縣",
-      "臺東縣",
-      "澎湖縣",
-      "金門縣",
-      "連江縣"
-];
-
-const DISTRICTS = {
-    "臺北市": [
-        "中正區",
-        "大同區",
-        "中山區",
-        "萬華區",
-        "信義區",
-        "松山區",
-        "大安區",
-        "南港區",
-        "北投區",
-        "內湖區",
-        "士林區",
-        "文山區",
-      ],
-      "新北市": [
-        "板橋區",
-        "新莊區",
-        "泰山區",
-        "林口區",
-        "淡水區",
-        "金山區",
-        "八里區",
-        "萬里區",
-        "石門區",
-        "三芝區",
-        "瑞芳區",
-        "汐止區",
-        "平溪區",
-        "貢寮區",
-        "雙溪區",
-        "深坑區",
-        "石碇區",
-        "新店區",
-        "坪林區",
-        "烏來區",
-        "中和區",
-        "永和區",
-        "土城區",
-        "三峽區",
-        "樹林區",
-        "鶯歌區",
-        "三重區",
-        "蘆洲區",
-        "五股區",
-      ],
-      "基隆市": [
-        "仁愛區",
-        "中正區",
-        "信義區",
-        "中山區",
-        "安樂區",
-        "暖暖區",
-        "七堵區",
-      ],
-      "桃園市": [
-        "桃園區",
-        "中壢區",
-        "平鎮區",
-        "八德區",
-        "楊梅區",
-        "蘆竹區",
-        "龜山區",
-        "龍潭區",
-        "大溪區",
-        "大園區",
-        "觀音區",
-        "新屋區",
-        "復興區",
-      ],
-      "新竹縣": [
-        "竹北市",
-        "竹東鎮",
-        "新埔鎮",
-        "關西鎮",
-        "峨眉鄉",
-        "寶山鄉",
-        "北埔鄉",
-        "橫山鄉",
-        "芎林鄉",
-        "湖口鄉",
-        "新豐鄉",
-        "尖石鄉",
-        "五峰鄉",
-      ],
-      "新竹市": ["東區", "北區", "香山區"],
-      "苗栗縣": [
-        "苗栗市",
-        "通霄鎮",
-        "苑裡鎮",
-        "竹南鎮",
-        "頭份鎮",
-        "後龍鎮",
-        "卓蘭鎮",
-        "西湖鄉",
-        "頭屋鄉",
-        "公館鄉",
-        "銅鑼鄉",
-        "三義鄉",
-        "造橋鄉",
-        "三灣鄉",
-        "南庄鄉",
-        "大湖鄉",
-        "獅潭鄉",
-        "泰安鄉",
-      ],
-      "臺中市": [
-        "中區",
-        "東區",
-        "南區",
-        "西區",
-        "北區",
-        "北屯區",
-        "西屯區",
-        "南屯區",
-        "太平區",
-        "大里區",
-        "霧峰區",
-        "烏日區",
-        "豐原區",
-        "后里區",
-        "東勢區",
-        "石岡區",
-        "新社區",
-        "和平區",
-        "神岡區",
-        "潭子區",
-        "大雅區",
-        "大肚區",
-        "龍井區",
-        "沙鹿區",
-        "梧棲區",
-        "清水區",
-        "大甲區",
-        "外埔區",
-        "大安區",
-      ],
-      "南投縣": [
-        "南投市",
-        "埔里鎮",
-        "草屯鎮",
-        "竹山鎮",
-        "集集鎮",
-        "名間鄉",
-        "鹿谷鄉",
-        "中寮鄉",
-        "魚池鄉",
-        "國姓鄉",
-        "水里鄉",
-        "信義鄉",
-        "仁愛鄉",
-      ],
-      "彰化縣": [
-        "彰化市",
-        "員林鎮",
-        "和美鎮",
-        "鹿港鎮",
-        "溪湖鎮",
-        "二林鎮",
-        "田中鎮",
-        "北斗鎮",
-        "花壇鄉",
-        "芬園鄉",
-        "大村鄉",
-        "永靖鄉",
-        "伸港鄉",
-        "線西鄉",
-        "福興鄉",
-        "秀水鄉",
-        "埔心鄉",
-        "埔鹽鄉",
-        "大城鄉",
-        "芳苑鄉",
-        "竹塘鄉",
-        "社頭鄉",
-        "二水鄉",
-        "田尾鄉",
-        "埤頭鄉",
-        "溪州鄉",
-      ],
-      "雲林縣": [
-        "斗六市",
-        "斗南鎮",
-        "虎尾鎮",
-        "西螺鎮",
-        "土庫鎮",
-        "北港鎮",
-        "莿桐鄉",
-        "林內鄉",
-        "古坑鄉",
-        "大埤鄉",
-        "崙背鄉",
-        "二崙鄉",
-        "麥寮鄉",
-        "臺西鄉",
-        "東勢鄉",
-        "褒忠鄉",
-        "四湖鄉",
-        "口湖鄉",
-        "水林鄉",
-        "元長鄉",
-      ],
-      "嘉義縣": [
-        "太保市",
-        "朴子市",
-        "布袋鎮",
-        "大林鎮",
-        "民雄鄉",
-        "溪口鄉",
-        "新港鄉",
-        "六腳鄉",
-        "東石鄉",
-        "義竹鄉",
-        "鹿草鄉",
-        "水上鄉",
-        "中埔鄉",
-        "竹崎鄉",
-        "梅山鄉",
-        "番路鄉",
-        "大埔鄉",
-        "阿里山鄉",
-      ],
-      "嘉義市": ["東區", "西區"],
-      "臺南市": [
-        "中西區",
-        "東區",
-        "南區",
-        "北區",
-        "安平區",
-        "安南區",
-        "永康區",
-        "歸仁區",
-        "新化區",
-        "左鎮區",
-        "玉井區",
-        "楠西區",
-        "南化區",
-        "仁德區",
-        "關廟區",
-        "龍崎區",
-        "官田區",
-        "麻豆區",
-        "佳里區",
-        "西港區",
-        "七股區",
-        "將軍區",
-        "學甲區",
-        "北門區",
-        "新營區",
-        "後壁區",
-        "白河區",
-        "東山區",
-        "六甲區",
-        "下營區",
-        "柳營區",
-        "鹽水區",
-        "善化區",
-        "大內區",
-        "山上區",
-        "新市區",
-        "安定區",
-      ],
-      "高雄市": [
-        "楠梓區",
-        "左營區",
-        "鼓山區",
-        "三民區",
-        "鹽埕區",
-        "前金區",
-        "新興區",
-        "苓雅區",
-        "前鎮區",
-        "小港區",
-        "旗津區",
-        "鳳山區",
-        "大寮區",
-        "鳥松區",
-        "林園區",
-        "仁武區",
-        "大樹區",
-        "大社區",
-        "岡山區",
-        "路竹區",
-        "橋頭區",
-        "梓官區",
-        "彌陀區",
-        "永安區",
-        "燕巢區",
-        "田寮區",
-        "阿蓮區",
-        "茄萣區",
-        "湖內區",
-        "旗山區",
-        "美濃區",
-        "內門區",
-        "杉林區",
-        "甲仙區",
-        "六龜區",
-        "茂林區",
-        "桃源區",
-        "那瑪夏區",
-      ],
-      "屏東縣": [
-        "屏東市",
-        "潮州鎮",
-        "東港鎮",
-        "恆春鎮",
-        "萬丹鄉",
-        "長治鄉",
-        "麟洛鄉",
-        "九如鄉",
-        "里港鄉",
-        "鹽埔鄉",
-        "高樹鄉",
-        "萬巒鄉",
-        "內埔鄉",
-        "竹田鄉",
-        "新埤鄉",
-        "枋寮鄉",
-        "新園鄉",
-        "崁頂鄉",
-        "林邊鄉",
-        "南州鄉",
-        "佳冬鄉",
-        "琉球鄉",
-        "車城鄉",
-        "滿州鄉",
-        "枋山鄉",
-        "霧台鄉",
-        "瑪家鄉",
-        "泰武鄉",
-        "來義鄉",
-        "春日鄉",
-        "獅子鄉",
-        "牡丹鄉",
-        "三地門鄉",
-      ],
-      "宜蘭縣": [
-        "宜蘭市",
-        "羅東鎮",
-        "蘇澳鎮",
-        "頭城鎮",
-        "礁溪鄉",
-        "壯圍鄉",
-        "員山鄉",
-        "冬山鄉",
-        "五結鄉",
-        "三星鄉",
-        "大同鄉",
-        "南澳鄉",
-      ],
-      "花蓮縣": [
-        "花蓮市",
-        "鳳林鎮",
-        "玉里鎮",
-        "新城鄉",
-        "吉安鄉",
-        "壽豐鄉",
-        "秀林鄉",
-        "光復鄉",
-        "豐濱鄉",
-        "瑞穗鄉",
-        "萬榮鄉",
-        "富里鄉",
-        "卓溪鄉",
-      ],
-      "臺東縣": [
-        "臺東市",
-        "成功鎮",
-        "關山鎮",
-        "長濱鄉",
-        "海端鄉",
-        "池上鄉",
-        "東河鄉",
-        "鹿野鄉",
-        "延平鄉",
-        "卑南鄉",
-        "金峰鄉",
-        "大武鄉",
-        "達仁鄉",
-        "綠島鄉",
-        "蘭嶼鄉",
-        "太麻里鄉",
-      ],
-      "澎湖縣": ["馬公市", "湖西鄉", "白沙鄉", "西嶼鄉", "望安鄉", "七美鄉"],
-      "金門縣": ["金城鎮", "金湖鎮", "金沙鎮", "金寧鄉", "烈嶼鄉", "烏坵鄉"],
-      "連江縣": ["南竿鄉", "北竿鄉", "莒光鄉", "東引鄉"]
-};
+import axios from "axios";
+import { CITIES, DISTRICTS } from "./taiwanCityDistricts.js"
 
 const CATEGORIES = ["highRatings", "serves_beer", "serves_brunch", "serves_dinner", "takeout"];
+
+const googleSearchApiKey = process.env.GOOGLE_MAP_API_KEY;
 
 async function expandDataset() {
   for (const city of CITIES) {
     for (const district of DISTRICTS[city]) {
-      
+      console.log(`searching ${city}, ${district}`);
+      const cafes = await getNearByCafes(city, district);
+      await categorize(city, district, cafes);
+    }
+  }
+}
+
+async function getNearByCafes(city, district) {
+  // 1. 把縣市切成小方塊
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}${district}&language=zh-TW&key=${googleSearchApiKey}`;
+  const result = await axios.get(url);
+  const data = result.data;
+
+  if (data.status !== 'OK') {
+    throw new Error (`error: status: ${data.status}`);
+  }
+
+  const res = data.results;
+  const sw = res.geometry.viewport.southwest;
+  const ne = res.geometry.viewport.northeast;
+
+  const grid = generateGridPoints(sw, ne, 1000);
+  console.log(`generate ${grid.count} grid point`);
+
+  // 2. 搜尋每個小方塊
+  const allCafes = new Set();
+  const seen = new Set();
+  for (const point of grid) {
+    const cafeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${point[0]},${point[1]}&radius=1000&keyword=咖啡&language=zh-TW&key=${googleSearchApiKey}`;
+    const cafeRes = await axios.get(cafeUrl);
+    const cafes = cafeRes.results;
+
+    for (const cafe of cafes) {
+      if (!seen.has(cafe.place_id)) {
+        seen.add(cafe.place_id);
+        allCafes.add(cafe);
+      }
     }
   }
 
+  // 3. 回傳透過小方塊找到的每間咖啡廳
+  return allCafes;
+}
 
+/**
+ * 依據經緯度邊界產生格子狀查詢點（每個為中心點）
+ * @param {Object} sw - 南西角 { lat, lng }
+ * @param {Object} ne - 東北角 { lat, lng }
+ * @param {Number} stepMeters - 每格距離，單位：公尺（預設 1000m）
+ * @returns {Array<Object>} - 格子的中心點 [{ lat, lng }, ...]
+ */
+function generateGridPoints(sw, ne, stepMeters = 1000) {
+  const points = [];
 
-  try {
-    const docRef = await addDoc(collection(db, "testUsers"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+  const metersToLat = (meters) => meters / 111_000;
+  const metersToLng = (meters, latitude) =>
+    meters / (111_000 * Math.cos((latitude * Math.PI) / 180));
+
+  const midLat = (sw.lat + ne.lat) / 2;
+  const deltaLat = metersToLat(stepMeters);
+  const deltaLng = metersToLng(stepMeters, midLat);
+
+  for (let lat = sw.lat; lat <= ne.lat; lat += deltaLat) {
+    for (let lng = sw.lng; lng <= ne.lng; lng += deltaLng) {
+      points.push({ lat: Number(lat.toFixed(6)), lng: Number(lng.toFixed(6)) });
+    }
+  }
+
+  return points;
+}
+
+// isQualifiedForCategory
+async function categorize(city, district, cafes) {
+  for (const cafe of cafes) {
+    
+    for (const category of CATEGORIES) {
+      const cafeRef = db.collection(category).doc(city).collection(district).doc(`${cafe.name}_${cafe.place_id}`);
+
+      const doc = await cafeRef.get();
+      if (doc.exists) {
+        console.lof(`${cafe.name} exists`);
+        return;
+      }
+
+      // 該咖啡廳不存在資料庫，使用details搜尋，並加入資料庫
+      await addToDataset(city, district, cafe, category, cafeRef);
+    }
   }
 }
+
+async function addToDataset(city, district, cafe, category, cafeRef) {
+  const detailUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${cafe.place_id}&language=zh-TW&key=${googleSearchApiKey}`;
+  const res = await axios.get(detailUrl);
+  const data = res.result;
+
+  if (!(category === 'highRatings' && cafe.rating >= 4.3)) { return; } 
+  if (!(category === 'serves_beer' && data.serves_beer)) { return; }
+  if (!(category === 'serves_brunch' && data.serves_lunch)) { return; }
+  if (!(category === 'takeout' && data.takeout)) { return; }
+
+  const cafeData = buildCafeData(city, district, data);
+  cafeRef.set(cafeData);
+}
+
+function buildCafeData(city, district, data) {
+  const cafeData = {
+    city: city,
+    district: district,
+    formatted_address: data.formatted_address,
+    formatted_phone_number: data.formatted_phone_number ?? "",
+    name: data.name,
+    rating: Math.round(data.rating ?? 0),
+    price_level: data.price_level ?? null,
+    services: {
+      serves_beer: data.serves_beer ?? false,
+      serves_breakfast: data.serves_breakfast ?? false,
+      serves_brunch: data.serves_brunch ?? false,
+      serves_dinner: data.serves_dinner ?? false,
+      serves_lunch: data.serves_lunch ?? false,
+      serves_wine: data.serves_wine ?? false,
+      takeout: data.takeout ?? false
+    },
+    types: data.types ?? [],
+    user_rating_total: data.user_ratings_total ?? 0,
+    vicinity: data.vicinity ?? "",
+    place_id: data.place_id,
+    weekday_text: data.opening_hours?.weekday_text ?? ["not provided"],
+    reviews: (data.reviews ?? []).map((r) => ({
+      reviewer_name: r.author_name,
+      reviewer_rating: r.rating,
+      review_text: r.text,
+      review_time: r.relative_time_description
+    }))
+  };  
+
+  return cafeData;
+}
+
 
 await expandDataset();
